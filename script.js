b398b1deca58cf7e2a83853a16c332dae5bf6f2a
huymai2k8 <166737353+huymai2k8@users.noreply.github.com>
2024-04-11 22:53:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function dulich() {
+    window.open("https://www.vntrip.vn/cam-nang/cac-khu-du-lich-o-ben-tre-18434");
+  }
+  
+function dacsan() {
+    window.open("https://www.ivivu.com/blog/2023/05/12-mon-dac-san-ben-tre-gay-sot-thuc-khach/");
+  }
+  
+function langnghe() {
+    window.open("https://mytour.vn/vi/blog/bai-viet/top-5-lang-nghe-truyen-thong-ben-tre-tuyet-voi-don-dau-su-sang-tao-tu-mytourcom.html");
+  }
